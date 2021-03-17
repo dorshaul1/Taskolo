776b2bdc1e907d548c1b51f3fd62822c5a17fad7
dorshaul1 <dor.asti@gmail.com>
2021-03-17 15:18:19 +0200

    bf

--- src/cmps/board/board-header.vue
+++ src/cmps/board/board-header.vue
@@ -1,12 +1,47 @@
 <template>
   <header class="board-header">
-    <button class="boards-btn">Boards</button>
+    <!-- <button class="boards-btn">Boards</button> -->
+    <el-select v-model="value" placeholder="Select">
+      <el-option
+        v-for="board in getBoards"
+        :key="board._id"
+        :label="board.name"
+        :value="board._id"
+      >
+      </el-option>
+    </el-select>
   </header>
 </template>
 
 <script>
 export default {
   name: "board-header",
+   data() {
+      return {
+        // options: [{
+        //   value: 'Option1',
+        //   label: 'Option1'
+        // }, {
+        //   value: 'Option2',
+        //   label: 'Option2'
+        // }, {
+        //   value: 'Option3',
+        //   label: 'Option3'
+        // }, {
+        //   value: 'Option4',
+        //   label: 'Option4'
+        // }, {
+        //   value: 'Option5',
+        //   label: 'Option5'
+        // }],
+        value: ''
+      }
+    },
+    computed:{
+      getBoards(){
+        return this.$store.getters.boards
+      }
+    }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './registerServiceWorker'
 import router from './router';
 import {store} from './store'
 import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui';
 
 import './styles/main.scss'
 
