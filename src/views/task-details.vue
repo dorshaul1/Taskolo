09d93e12e24031c9b543b4a6b3f213fa04489f94
Michael Peer <michaelpeer2004@gmail.com>
2021-03-17 22:00:49 +0200

    f

--- src/views/task-details.vue
+++ src/views/task-details.vue
@@ -1,12 +1,12 @@
 <template>
-    <section class="task-details-container">
+    <section v-if="task" class="task-details-container">
         <div class="cover">
             <a class="change-cover" href="#">Cover</a>
             <a class="close-modal-btn" href="#">X</a>
         </div>
 
         <div class="title flex align-start column">
-            <h1>Finish Tech Design</h1>
+            <h1>{{task.title}}</h1>
             <h4>in list To Do</h4>
         </div>
 
@@ -163,10 +163,12 @@ export default {
     computed: {
         task() {
             return this.$store.getters.currTask
+        },
+        boardName() {
+            return this.$store.getters.boardName
         }
     },
     created() {
-        console.log(this.$store.state.currTask, "createddd")
     }
 };
 </script>
