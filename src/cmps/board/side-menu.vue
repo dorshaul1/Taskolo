47f39302203684af8f8086ff0f39ac7e5c67fe99
dorshaul1 <dor.asti@gmail.com>
2021-03-17 20:36:56 +0200

    side-bar

--- src/cmps/board/side-menu.vue
+++ src/cmps/board/side-menu.vue
@@ -1,15 +1,20 @@
 <template>
-  <!-- <section class="side-menu"> -->
-    <!-- <div class="side-menu-screen"></div> -->
-    <section class="side-menu-container flex column"></section>
+  <section class="side-menu-container flex column">
+    <button @click="closeMenu" class="close-menu">X</button>
+    <section class="side-menu-content">
+      <h1>Menu</h1>
+    </section>
+  </section>
   <!-- </section> -->
 </template>
 
 <script>
 export default {
   name: "side-menu",
+  methods: {
+    closeMenu() {
+      this.$emit("close");
+    },
+  },
 };
 </script>
-
-<style>
-</style>
