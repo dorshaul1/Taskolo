190e3d21ac45bad514c5089d4ade52ba70721140
dorshaul1 <dor.asti@gmail.com>
2021-03-18 21:24:43 +0200

    due date

--- src/cmps/task/task-option/task-details-previews/due-date-preview.vue
+++ src/cmps/task/task-option/task-details-previews/due-date-preview.vue
@@ -1,10 +1,27 @@
 <template>
   <section class="due-date-preview flex column align-start">
-    <h3>Due Date</h3>
-    <div class="date-preview" @click="openDueDateModal">
+    <div
+      v-if="date.start"
+      class="date-preview-range flex align-start"
+      @click="openDueDateModal"
+    >
+      <div class="start-date flex column align-start">
+        <h3>start Date</h3>
+        {{ dateToShow.start | moment("dddd, MMMM Do YYYY, h:mm a") }}
+      </div>
+      <div class="end-date flex column align-start">
+        <h3>Due Date</h3>
+        {{ dateToShow.end | moment("dddd, MMMM Do YYYY, h:mm a") }}
+      </div>
+    </div>
+    <div
+      v-else
+      class="date-preview flex column align-start"
+      @click="openDueDateModal"
+    >
+      <h3>Due Date</h3>
       {{ dateToShow | moment("dddd, MMMM Do YYYY, h:mm a") }}
     </div>
-    <!-- <div v-else class="date-preview" @click="openDueDateModal">rrr</div> -->
   </section>
 </template>
 
@@ -19,16 +36,20 @@ export default {
       console.log("this.date:", this.date);
       return this.date;
     },
+    // checkIfRange() {
+    //   console.log(' typeof this.date:',  typeof this.date)
+    //   return typeof this.date === String;
+    // },
   },
   methods: {
     openDueDateModal() {
       this.$emit("setDate");
     },
-    checkIfRange() {
-      const type = typeof this.date;
-      console.log("type:", type);
-    //   return typeof this.date === String;
-    },
+    // checkIfRange() {
+    //   const type = typeof this.date;
+    //   console.log("type:", type);
+    //   //   return typeof this.date === String;
+    // },
   },
 };
 </script>
