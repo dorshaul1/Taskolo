a47ee6cbd6afbbfd636843573e74e1a0c0b41646
YtavPeer <ytav.peer@gmail.com>
2021-03-18 15:08:33 +0200

    v7

--- src/views/board-details.vue
+++ src/views/board-details.vue
@@ -33,8 +33,8 @@
         </textarea>
 
         <div class="flex space-between">
-          <button @click="addNewTask" class="add-task">Add card</button>
-          <button @click="closeTaskAdd">x</button>
+          <!-- <button @click="addNewTask" class="add-task">Add card</button>
+          <button @click="closeTaskAdd">x</button> -->
         </div>
       </section>
     </main>
@@ -79,6 +79,9 @@ export default {
     openGroupkAdd() {
       this.isTakeGroup = true;
     },
+    addNewTask(){
+      console.log("addind new task")
+    }
   },
   watch: {
     boardId: {
