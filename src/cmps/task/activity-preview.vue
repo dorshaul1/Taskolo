65d7bdcf554f462f3f525302c073926df33279a1
dorshaul1 <dor.asti@gmail.com>
2021-03-19 17:15:33 +0200

    morning

--- src/cmps/task/activity-preview.vue
+++ src/cmps/task/activity-preview.vue
@@ -1,16 +1,22 @@
 <template>
-    <section class="activity flex column">
-        <div class="acitivty-title flex space-between align-center">
-            <h3>Activity</h3>
-            <button>Show Details</button>
-        </div>
-        <input type="text" placeholder="Wtire a comment..." />
-    </section>
+  <section class="activity flex column">
+
+    <img
+      class="task-prev-icon"
+      src="@/assets/task-icon/bullet-list.png"
+      alt=""
+    />
+    <div class="acitivty-title flex space-between align-center">
+      <h3>Activity</h3>
+      <button>Show Details</button>
+    </div>
+    <input type="text" placeholder="Wtire a comment..." />
+  </section>
 </template>
 
 <script>
 export default {
-    name: "activity-preview",
+  name: "activity-preview",
 };
 </script>
 
