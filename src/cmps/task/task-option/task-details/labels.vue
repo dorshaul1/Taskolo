84254003f2c5cab11efc3ffc44169b5f1b789fd7
Michael Peer <michaelpeer2004@gmail.com>
2021-03-19 15:32:54 +0200

    g

--- src/cmps/task/task-option/task-details/labels.vue
+++ src/cmps/task/task-option/task-details/labels.vue
@@ -4,7 +4,7 @@
         <h3>Labels</h3>
         <div v-for="label in labels" :key="label.id" class="flex">
             <span class="label" :style="{backgroundColor: label.color}">{{label.title}}</span>
-            <span>Edit</span>
+            <span @click="openLabelsEdit">Edit</span>
         </div>
 
         <button>Create a new label</button>
@@ -16,6 +16,11 @@ export default {
     name: "labels",
     props: {
       labels:{}
+    },
+    methods: {
+      openLabelsEdit() {
+        this.$emit('open-label-edit')
+      }
     },
     created() {
       console.log(this.labels)
