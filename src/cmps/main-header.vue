8fa2133d5429d7c2fa5a00f507f86ec326a7dfa8
dorshaul1 <dor.asti@gmail.com>
2021-03-21 11:36:27 +0200

    morning

--- src/cmps/main-header.vue
+++ src/cmps/main-header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="app-header">
+  <header class="app-header flex align-center space-between">
     <section class="app-header-container flex align-center space-between">
     <nav class="flex align-center">
       <button class="home-btn flex center" @click="$router.push('/')">
@@ -13,12 +13,12 @@
       <img src="../assets/logo/logo-txt-white.png" alt="" />
       <!-- <img src="../assets/logo/logo-full.png" alt=""> -->
     </section>
+    </section>
     <section class="profile-details">
       <!-- <div class="member-avatar"> -->
         <img class="member-avatar" src="https://randomuser.me/api/portraits/thumb/men/75.jpg" alt="">
       <!-- </div> -->
       </section>
-    </section>
   </header>
 </template>
 
