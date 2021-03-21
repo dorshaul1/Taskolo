020018f5c305a12d7dd49013598ffb264e2b6e6e
YtavPeer <ytav.peer@gmail.com>
2021-03-21 18:08:30 +0200

    fdsa

--- src/main.js
+++ src/main.js
@@ -5,6 +5,14 @@ import router from './router';
 import { store } from './store'
 import 'element-ui/lib/theme-chalk/index.css';
 import ElementUI from 'element-ui';
+//font awesome
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import './styles/main.scss'
 
@@ -15,6 +23,11 @@ import './styles/main.scss'
 // Vue.component('calendar', Calendar)
 // Vue.component('date-picker', DatePicker)
 Vue.use(require('vue-moment'));
+
+
+library.add(fab, fas, far);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 import VCalendar from 'v-calendar';
 Vue.use(VCalendar)
