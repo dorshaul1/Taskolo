eac21c18c42c929dc1c78785b9b81e1a93f7708c
Michael Peer <michaelpeer2004@gmail.com>
2021-03-22 14:56:34 +0200

    g

--- src/cmps/task/task-option/task-details/move-card.vue
+++ src/cmps/task/task-option/task-details/move-card.vue
@@ -12,8 +12,22 @@
             ></el-option>
         </el-select>
         <div class="select-container flex">
-            <!-- <el-select></el-select>
-            <el-select></el-select> -->
+            <el-select v-model="copyTo.group">
+                <el-option
+                    v-for="group in selectedBoard.groups"
+                    :key="group.id"
+                    :label="group.title"
+                    :value="group.id"
+                ></el-option>
+            </el-select>
+            <el-select v-model="copyTo.position">
+                <el-option
+                    v-for="idx in taskCount"
+                    :key="idx"
+                    :label="idx"
+                    :value="idx"
+                ></el-option>
+            </el-select>
         </div>
     </section>
 </template>
@@ -24,13 +38,37 @@ export default {
         boards: {
             type: Array,
         },
+        currBoard: {
+            type: Object,
+        },
+        currGroup: {
+            type: Object,
+        },
     },
     data() {
         return {
             copyTo: {
                 board: null,
+                group: null,
+                position: null,
             },
         };
+    },
+    computed: {
+        selectedBoard() {
+            if (!this.copyTo.board) return this.currBoard;
+            const selectedBoard = this.boards.find(
+                (board) => board._id === this.copyTo.board
+            );
+            return selectedBoard;
+        },
+        taskCount() {
+            if (!this.copyTo.group) return this.currGroup.tasks.length;
+            const selectedGroup = this.selectedBoard.groups.find(
+                (group) => group._id === !this.copyTo.group.id
+            );
+            return selectedGroup.tasks.length;
+        },
     },
 
     created() {
