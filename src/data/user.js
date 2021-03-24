7176d2dd45ed49a9edf3d8f019f84d16597c44f1
dorshaul1 <dor.asti@gmail.com>
2021-03-24 13:30:29 +0200

    dsv

--- src/data/user.js
+++ src/data/user.js
@@ -1,55 +1,47 @@
 
 
 export const user = [{
-      "_id": "u101",
       "fullname": "Randy Kelly",
       "username": "silverswan131",
       "password": "firewall",
       "imgUrl": "https://randomuser.me/api/portraits/thumb/men/75.jpg",
 },
 {
-      "_id": "u102",
       "fullname": "Jill Chambers",
       "username": "jilly",
       "password": "secret",
       "imgUrl": "https://randomuser.me/api/portraits/thumb/women/44.jpg",
 }, {
-      "_id": "u103",
       "fullname": "Gloria Price",
       "username": "glori",
       "password": "secret",
       "imgUrl": "https://randomuser.me/api/portraits/thumb/women/25.jpg",
 },
 {
-      "_id": "u104",
       "fullname": "Tamara Perez",
       "username": "tami",
       "password": "secret",
       "imgUrl": "https://randomuser.me/api/portraits/thumb/women/14.jpg",
 },
 {
-      "_id": "u105",
       "fullname": "Roberto Fox",
       "username": "robli",
       "password": "secret",
       "imgUrl": "https://randomuser.me/api/portraits/thumb/men/53.jpg",
 },
 {
-      "_id": "u106",
       "fullname": "Johnny Stephens",
       "username": "jhon jhon",
       "password": "secret",
       "imgUrl": "https://randomuser.me/api/portraits/thumb/men/31.jpg",
 },
 {
-      "_id": "u107",
       "fullname": "Ella Gregory",
       "username": "elliy",
       "password": "secret",
       "imgUrl": "https://randomuser.me/api/portraits/thumb/women/19.jpg",
 },
 {
-      "_id": "u108",
       "fullname": "Madison Woods",
       "username": "madli",
       "password": "secret",
