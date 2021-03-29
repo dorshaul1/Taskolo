489586d409141e5e7929c4d86f55d3201f7b37cb
Michael Peer <michaelpeer2004@gmail.com>
2021-03-29 09:28:05 +0300

    g

--- src/cmps/task/activity-preview.vue
+++ src/cmps/task/activity-preview.vue
@@ -3,17 +3,26 @@
         <img class="activity-icon" src="@/assets/task-icon/list.svg" alt="" />
         <div class="acitivty-title flex space-between align-center">
             <h3>Activity</h3>
-            <button>Show Details</button>
+            <button @click="toggleShowDetails">Show Details</button>
         </div>
         <input type="text" placeholder="Wtire a comment..." />
 
-        <section class="activity-item flex flex-start" v-for="activity in activites" :key="activity.id">
-            <img class="profile" :src="activity.byMember.imgUrl"/>
-            <div class="activity-data">
-                <div><span>{{activity.byMember.fullname}}</span>{{activity.txt}}</div>
-                <a href="#">{{formatDate(activity.createdAt)}}</a>
-            </div>
-        </section>
+        <div v-if="isShowDetailsOpen">
+            <section
+                class="activity-item flex flex-start"
+                v-for="activity in activites"
+                :key="activity.id"
+            >
+                <img class="profile" :src="activity.byMember.imgUrl" />
+                <div class="activity-data">
+                    <div>
+                        <span>{{ activity.byMember.fullname }}</span
+                        >{{ activity.txt }}
+                    </div>
+                    <a href="#">{{ formatDate(activity.createdAt) }}</a>
+                </div>
+            </section>
+        </div>
     </section>
 </template>
 
@@ -23,13 +32,21 @@ export default {
     props: {
         activites: { type: Array }
     },
+    data() {
+        return {
+            isShowDetailsOpen: true
+        }
+    },
     methods: {
-      formatDate(data) {
-        return this.$moment(data).format('lll')
-      }
+        formatDate(data) {
+            return this.$moment(data).format('lll')
+        },
+        toggleShowDetails() {
+            this.isShowDetailsOpen = !this.isShowDetailsOpen
+        }
     },
     created() {
-      console.log('activity', this.activites)
+        console.log('activity', this.activites)
     }
 };
 </script>
