ce6634381e72e511b34ab36a6715c0054f1b56e5
YtavPeer <ytav.peer@gmail.com>
2021-03-30 18:29:07 +0300

    dsasafsd

--- src/cmps/board/board-header.vue
+++ src/cmps/board/board-header.vue
@@ -45,6 +45,10 @@
     <button class="show-menu-btn" @click="openMenu">
       <i class="el-icon-more"></i> Show Menu
     </button>
+
+    <button class="show-menu-btn" @click="openMenu">
+      <i class="el-icon-more"></i> 
+    </button>
   </header>
 </template>
 
@@ -80,7 +84,7 @@ export default {
       return this.$store.getters.users;
     },
     currBoard() {
-      return this.$clone(this.$store.getters.currBoard)
+      return this.$clone(this.$store.getters.currBoard);
     },
   },
   methods: {
@@ -97,7 +101,7 @@ export default {
     },
 
     openMenu() {
-      this.$emit("open"); 
+      this.$emit("open");
     },
   },
   components: {
